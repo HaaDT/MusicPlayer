9ad9a3d1ef31831233ca6bc46ef1c940122b75b2
Haadt <55682433+HaaDT@users.noreply.github.com>
2023-04-13 16:53:49 -0400

    idefhg0hedihgi0er
    34wt43t

--- WpfMusicPlayers/MainWindow.xaml.cs
+++ WpfMusicPlayers/MainWindow.xaml.cs
@@ -1,27 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using WpfMusicPlayers.cs;
 
 namespace WpfMusicPlayers
 {
     public partial class MainWindow : Window
     {
+
+        MediaPlayer player = new MediaPlayer();
+        bool ativo = false;
+        static List<musica> osts = new List<musica>();
+        int numero = 1;
+        public void adicionar(string a, string b, string c, string d, string e)
+        {
+            musica ost = new musica();
+            ost.path = a;
+            ost.icon = b;
+            ost.nome = c;
+            ost.artista = d;
+            ost.tempo = e;
+            osts.Add(ost);
+        }
+
         public MainWindow()
         {
             InitializeComponent();
         }
-        
+
         //Botão de fechar o Aplicativo
         private void btFechar_Click(object sender, RoutedEventArgs e)
         {
@@ -32,49 +40,63 @@ namespace WpfMusicPlayers
         //Botão de voltar uma Música
         private void btVoltarMusica_Click(object sender, RoutedEventArgs e)
         {
-
+            if (numero > 1) numero--;
         }
 
         //Botão de avançar uma Música
         private void btAvancarMusica_Click(object sender, RoutedEventArgs e)
         {
-
+            if (numero < 5) numero++;
         }
 
         //Botão que alterna entre pausar e tocar as Músics
         private void btPausarETocar_Click(object sender, RoutedEventArgs e)
         {
-
+            if (!ativo)
+            {
+                player.Play();
+                ativo = true;
+            }
+            else
+            {
+                player.Pause();
+                ativo = false;
+            }
         }
 
         //Botão que Toca a música 1
         private void musica1_Selected(object sender, RoutedEventArgs e)
         {
-
+            numero = 1;
+            imImagemTocando.Source = new BitmapImage(new Uri(@"/Imagens/icons/iconDp.png", UriKind.Relative));
         }
 
         //Botão que Toca a música 2
         private void musica2_Selected(object sender, RoutedEventArgs e)
         {
-
+            numero = 2;
+            imImagemTocando.Source = new BitmapImage(new Uri(@"/Imagens/icons/iconEsoc.jpg", UriKind.Relative));
         }
 
         //Botão que Toca a música 3
         private void musica3_Selected(object sender, RoutedEventArgs e)
         {
-
+            numero = 3;
+            imImagemTocando.Source = new BitmapImage(new Uri(@"/Imagens/icons/iconMl.jpg", UriKind.Relative));
         }
 
         //Botão que Toca a música 4
         private void musica4_Selected(object sender, RoutedEventArgs e)
         {
-
+            numero = 4;
+            imImagemTocando.Source = new BitmapImage(new Uri(@"/Imagens/icons/iconMs.jpeg", UriKind.Relative));
         }
 
         //Botão que Toca a música 5
         private void musica5_Selected(object sender, RoutedEventArgs e)
         {
-
+            numero = 5;
+            imImagemTocando.Source = new BitmapImage(new Uri(@"/Imagens/icons/iconTrodt.jpg", UriKind.Relative));
         }
     }
 }
---
+++ WpfMusicPlayers/cs/musica.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace WpfMusicPlayers.cs
+{
+    internal class musica
+    {
+        public string path { get; set; }
+        public string icon { get; set; }
+        public string nome { get; set; }
+        public string artista { get; set; }
+        public string tempo { get; set; }   
+    }
+}
